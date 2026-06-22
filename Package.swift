aa52522625dbcb05deb03b2f69e3a6df24427c59
Jacob <104758482+JaFu-py@users.noreply.github.com>
2026-06-22 19:28:30 +0100

    Update packages

--- Package.swift
+++ Package.swift
@@ -3,23 +3,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "Rack",
-    platforms: [
-        .macOS(.v14)
-    ],
-    products: [
-        .library(name: "RackMac", targets: ["RackMac"]),
-        .executable(name: "Rack", targets: ["RackUI"])
-    ],
-    targets: [
-        .target(
-            name: "RackMac",
-            path: "packages/mac/src"
-        ),
-        .executableTarget(
-            name: "RackUI",
-            dependencies: ["RackMac"],
-            path: "packages/ui/src"
-        )
-    ]
+  name: "Rack",
+  platforms: [
+    .macOS(.v14)
+  ],
+  products: [
+    .library(name: "RackUI", targets: ["RackUI"]),
+    .executable(name: "Rack", targets: ["RackMac"]),
+  ],
+  targets: [
+    .target(
+      name: "RackUI",
+      path: "packages/ui/src",
+      linkerSettings: [
+        .unsafeFlags(["-L", ".build/rust/debug", "-lrack_services"])
+      ]
+    ),
+    .executableTarget(
+      name: "RackMac",
+      dependencies: ["RackUI"],
+      path: "packages/mac/src"
+    ),
+  ]
 )
